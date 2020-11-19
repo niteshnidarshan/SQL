8ea7c23cf4929052ef0ae5eea3ab01048d9dc797
Nitesh Nidarshan <niteshfromkatihar@gmail.com>
2020-11-19 20:17:24 +0530

    spring rest with client by RestTemplate

---
+++ spring_shekhar_sir/shekhar_sir_apps_spring_boot_jpa/pms-rest/src/main/java/com/pms/PmsRestApplication.java
@@ -0,0 +1,13 @@
+package com.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsRestApplication.class, args);
+	}
+
+}
